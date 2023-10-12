65cfba93f85200701cda51a7cbbabeabaceecb14
Polina Shupenko <polina.shupenko@mail.ru>
2023-10-12 12:21:01 +0300

    commit proverka

--- src/Laba2/App.java
+++ src/Laba2/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main (String[] args){
         System.out.println("Hello World!");
         System.out.println("Another one commit");
+        System.out.println("Ура у меня автомат");
     }
 
 
