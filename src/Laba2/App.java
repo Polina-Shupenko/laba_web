c28610ffcbb6a4e2ae7570ec57118c603e793621
Polina Shupenko <polina.shupenko@mail.ru>
2023-10-12 11:46:35 +0300

    commit test

---
+++ src/Laba2/App.java
@@ -0,0 +1,10 @@
+package Laba2;
+
+public class App {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Another one commit");
+    }
+
+
+}
--- src/Laba2/Main.java
+++ src/Laba2/Main.java
@@ -1,3 +1,5 @@
+package Laba2;
+
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello new programm");
